d6cbf53b312d363665bf23666ae0011dc81932e7
Maksim Chesnov <machesnov@gmail.com>
2024-12-07 13:25:35 +0300

    Add quotes URL

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -8,6 +8,29 @@ about_me = {
     "email":"machesnov@gmail.com"
 }
 
+quotes = [
+   {
+       "id": 3,
+       "author": "Rick Cook",
+       "text": "Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. Пока вселенная побеждает."
+   },
+   {
+       "id": 5,
+       "author": "Waldi Ravens",
+       "text": "Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках."
+   },
+   {
+       "id": 6,
+       "author": "Mosher’s Law of Software Engineering",
+       "text": "Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили."
+   },
+   {
+       "id": 8,
+       "author": "Yoggi Berra",
+       "text": "В теории, теория и практика неразделимы. На практике это не так."
+   }
+]
+
 @app.route("/") # Это первый URL, который мы будем обрабатывать
 def hello_world(): # Функция обработчик будет вызвана при запросе этого URL
     return "Hello, World!"
@@ -15,6 +38,14 @@ def hello_world(): # Функция обработчик будет вызван
 @app.route ("/about")
 def about():
     return about_me
+
+@app.route ("/quotes")
+def get_quotes():
+    return quotes
+
+#@app.route ("/quotes/<int:quotes_id>")
+#def quotes(quotes_id):
+    #return 'Quote %d' % quotes_id
 
 if __name__ == "__main__":
     app.run(debug=True)
